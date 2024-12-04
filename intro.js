beefedeb7592e42f109a5bcbe74c6180371c5c92
pratyushsingh5631 <74654228+pratyushsingh5631@users.noreply.github.com>
2024-12-04 19:41:45 +0530

    Add files via upload

---
+++ intro.js
@@ -0,0 +1,12 @@
+// let page = document.querySelector('body');
+// page.style.transition = "1s";
+
+// function change() {
+//     if (page.style.backgroundColor!="black") {
+//         page.style.backgroundColor = "black";
+//         page.style.color = "white";
+//     } else {
+//         page.style.backgroundColor = "white";
+//         page.style.color = "black";
+//     }
+// }
